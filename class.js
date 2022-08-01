f20330b7827b081a60807fce49a342e2f3a319f3
devkeywee <seoyoungwee@gmail.com>
2022-08-01 21:11:50 +0900

    Javascript6 Class(3)

--- class.js
+++ class.js
@@ -66,3 +66,74 @@ class Experiment
 const experiment = new Experiment();
 console.log(experiment.publicField);
 console.log(experiment.privateField);
+
+// 4. Static properties and methods
+// Too soon!
+class Article
+{
+    static publisher = 'Dream Coding';
+    constructor(articleNumber)
+    {
+        this.articleNumber = articleNumber;
+    }
+
+    static printPublisher()
+    {
+        console.log(Article.publisher);
+    }
+}
+
+const article1 = new Article(1);
+const article2 = new Article(2);
+console.log(Article.publisher);
+Article.printPublisher();
+
+// 5. Inheritance
+// a way for one class to extend another class.
+class Shape
+{
+    constructor(width, height, color)
+    {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    draw()
+    {
+        console.log(`drawing ${this.color} color of`);
+    }
+
+    getArea()
+    {
+        return this.width * this.height;
+    }
+}
+
+class Rectangle extends Shape {}
+class Triangle extends Shape 
+{
+    draw()
+    {
+        super.draw();
+        console.log('tri');
+    }
+    getArea()
+    {
+        return (this.width * this.height) / 2;
+    }
+}
+
+const rectangle = new Rectangle(20, 20, 'blue');
+rectangle.draw();
+console.log(rectangle.getArea());
+const triangle = new Triangle(20, 20, 'red');
+triangle.draw();
+console.log(triangle.getArea());
+
+// 6. Class checking: instanceOf
+console.log(rectangle instanceof Rectangle);
+console.log(triangle instanceof Rectangle);
+console.log(triangle instanceof Triangle);
+console.log(triangle instanceof Shape);
+console.log(triangle instanceof Object);
